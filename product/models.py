6824cd306360da6105c6e3ac0b817ef4125326dc
Mark Chukwuebuka <markedah9652@gmail.com>
2024-08-08 00:31:36 +0100

    logo

--- product/models.py
+++ product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from crm.models import BaseModel
+
 
 # Create your models here.
+# class Product(BaseModel):
+#     name = models.CharField(max_length=255)
+#     price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
