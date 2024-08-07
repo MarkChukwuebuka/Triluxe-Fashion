7b6522be24d29f6586276e993552df6f1191cd97
Mark Chukwuebuka <markedah9652@gmail.com>
2024-08-07 02:28:29 +0100

    added assets

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views import View
+
+from services.util import CustomRequestUtil
+
 
 # Create your views here.
+class HomeView(View, CustomRequestUtil):
+    template_name = "index.html"
+    extra_context_data = {
+        "title": "Welcome",
+    }
+
+    def get(self, request, *args, **kwargs):
+        return self.process_request(request)
+
+class ContactView(View, CustomRequestUtil):
+    template_name = "contact.html"
+    extra_context_data = {
+        "title": "Contact Us",
+    }
+
+    def get(self, request, *args, **kwargs):
+        return self.process_request(request)
+
+class AboutView(View, CustomRequestUtil):
+    template_name = "about.html"
+    extra_context_data = {
+        "title": "About Us",
+    }
+
+    def get(self, request, *args, **kwargs):
+        return self.process_request(request)
