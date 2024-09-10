91877ae9ea7cf27be4e027f30d73a31789fa0607
Mark Chukwuebuka <markedah9652@gmail.com>
2024-09-10 07:37:17 +0100

    update

--- core/settings.py
+++ core/settings.py
@@ -39,8 +39,6 @@ CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '')
 
 # CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in CSRF_TRUSTED_ORIGINS.split(',')] if CSRF_TRUSTED_ORIGINS else []
 
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
